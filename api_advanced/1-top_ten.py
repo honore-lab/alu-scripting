af2a34bcc1c80554ece0f024ba41034ffaa218c2
honore-lab <h.kamana@alustudent.com>
2026-07-21 21:48:25 +0200

    Robust handling for invalid subreddit or empty children in 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -8,9 +8,13 @@ def top_ten(subreddit):
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {'User-Agent': 'custom-agent/1.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json().get("data", {})
-        for post in data.get("children", []):
-            print(post.get("data", {}).get("title"))
-    else:
-        print(None)
+    if response.status_code != 200:
+        print("None")
+        return
+    data = response.json().get("data", {})
+    children = data.get("children", [])
+    if not children:
+        print("None")
+        return
+    for post in children:
+        print(post.get("data", {}).get("title"))
