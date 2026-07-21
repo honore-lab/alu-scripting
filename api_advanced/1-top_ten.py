b9ec4baa276c4e6647c7eb162700ab3076b80b99
honore-lab <h.kamana@alustudent.com>
2026-07-21 21:52:37 +0200

    Use unique User-Agent header for Reddit API in 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -6,7 +6,7 @@ import requests
 def top_ten(subreddit):
     """Print the first 10 hot posts for a given subreddit."""
     url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    headers = {'User-Agent': 'custom-agent/1.0'}
+    headers = {'User-Agent': 'alu-student:api_advanced:v1.0 (by /u/honore-lab)'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200:
         print(None)
